56cdadb7474619ac86043118772cbbe579fea4f6
Ryan Krauss <ryanwkrauss@gmail.com>
2016-07-15 16:46:09 -0400

    first commit

---
+++ I2C_SingleByte_loop.py
@@ -0,0 +1,28 @@
+#~ Former Student
+#~ November 23 2015
+
+import smbus
+import time
+
+bus = smbus.SMBus(1)
+SLAVE_ADDRESS = 0x04
+
+n = 3
+
+t1 = time.time()
+responses = []
+ilist = range(1,10)
+
+for i in ilist:
+    bus.write_byte(SLAVE_ADDRESS, i)
+    n_echo = bus.read_byte(SLAVE_ADDRESS)
+    responses.append(n_echo)
+    
+t2 = time.time()
+
+dt = t2-t1
+print 'responses = ' + str(responses)
+print 'dt = %0.4g' % dt	
+N = len(ilist)
+tpl = dt/N
+print('time per loop = %0.4g' % tpl)
